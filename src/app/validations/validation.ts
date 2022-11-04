c85b246b188095bbcae6c7c25e67ce3f005f1c22
sangeethSk <91061105+sangeethSk@users.noreply.github.com>
2022-11-05 01:35:16 +0530

    Update validation.ts

--- src/app/validations/validation.ts
+++ src/app/validations/validation.ts
@@ -1,21 +1,21 @@
-import { AbstractControl, ValidatorFn } from '@angular/forms';
+// import { AbstractControl, ValidatorFn } from '@angular/forms';
 
-export default class Validation {
-  static match(controlName: string, checkControlName: string): ValidatorFn {
-    return (controls: AbstractControl) => {
-      const control = controls.get(controlName);
-      const checkControl = controls.get(checkControlName);
+// export default class Validation {
+//   static match(controlName: string, checkControlName: string): ValidatorFn {
+//     return (controls: AbstractControl) => {
+//       const control = controls.get(controlName);
+//       const checkControl = controls.get(checkControlName);
 
-      if (checkControl.errors && !checkControl.errors.matching) {
-        return null;
-      }
+//       if (checkControl.errors && !checkControl.errors.matching) {
+//         return null;
+//       }
 
-      if (control.value !== checkControl.value) {
-        controls.get(checkControlName).setErrors({ matching: true });
-        return { matching: true };
-      } else {
-        return null;
-      }
-    };
-  }
-}
+//       if (control.value !== checkControl.value) {
+//         controls.get(checkControlName).setErrors({ matching: true });
+//         return { matching: true };
+//       } else {
+//         return null;
+//       }
+//     };
+//   }
+// }
